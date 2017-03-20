b965d66e3d9e435ec25e1823993d12979734f9c3
Tolu Idowu <itolu94@gmail.com>
2017-03-20 18:13:30 -0400

    Page fully func.  Just need to add final page that displays results and reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,48 @@
 var questionAndAnswer = [{
     question: "Who won the NBA finals in 2016",
     answers: [{
-        questions: "Cleveland Cavilairs",
-        result: false,
+        answer: "Cleveland Cavilairs",
+        isCorrect: true,
     }, {
-        questions: "Golden State Warriors",
-        result: false,
+        answer: "Golden State Warriors",
+        isCorrect: false,
     }, {
-        questions: "San Antonio Spurs",
-        result: false
+        answer: "San Antonio Spurs",
+        isCorrect: false
     }, {
-        questions: "Chicago Bulls",
-        result: true
-    }],
+        answer: "Chicago Bulls",
+        isCorrect: false
+    }, 'https://media.giphy.com/media/xT8qBd0tFdFV7l0gr6/giphy.gif']
 }, {
     question: "Who was the first Unanimous MVP",
     answers: [{
-        questions: "Stephan Curry",
-        result: false,
+        answer: "Stephan Curry",
+        isCorrect: false,
     }, {
-        questions: "Lebron James",
-        result: false,
+        answer: "Lebron James",
+        isCorrect: false,
     }, {
-        questions: "Derrick Rock",
-        result: false
+        answer: "Derrick Rock",
+        isCorrect: false
     }, {
-        questions: "Tom Brady",
-        result: true
-    }],
+        answer: "Tom Brady",
+        isCorrect: true
+    }, 'https://media.giphy.com/media/PuHp0vHXEGbUQ/giphy.gif' ]
 }, {
     question: "What was the last team to win consecutive championships ",
     answers: [{
-        questions: "Bulls",
-        result: false,
+        answer: "Bulls",
+        isCorrect: false,
     }, {
-        questions: "Lakers",
-        result: false,
+        answer: "Lakers",
+        isCorrect: false,
     }, {
-        questions: "Miami Heat",
-        result: false
+        answer: "Miami Heat",
+        isCorrect: false
     }, {
-        questions: "Golden State Warriors",
-        result: true
-    }]
+        answer: "Golden State Warriors",
+        isCorrect: true
+    }, 'https://media.giphy.com/media/l46Craj343bB2TMSA/giphy.gif']
 }];
 
 
@@ -51,59 +51,93 @@ var incorrectAnswers = 0;
 var indexHolder = 0;
 var timer;
 var intervalId;
+var currentQuestion;
 
+// when button is clicked, game begins. 
 $('#startGame').click(function() {
     displayQuestion();
+    $(this).off();
 });
 
 
-$('.usersChoices').on('click', function() {
+$(document).on('click', ".usersChoice", function() {
     console.log($(this).attr('value'));
-    if ($(this).attr('value') === true) {
+    if ($(this).attr('value') === 'true') {
+        clearInterval(intervalId);
+        correctGuess();
+    } else if ($(this).attr('value') === 'false') {
         clearInterval(intervalId);
         incorrectGuess();
-        console.log("correct");
-    } else if ($(this).attr('value') === false) {
-        clearInterval(intervalId);
-        correctAnswer()
-        console.log("Try again")
-    }
-
+    }     
 });
 
 
-
+// Displays the questions on screen
 function displayQuestion() {
+	$('.whiteBackground img').remove();
     $('#question').text(questionAndAnswer[indexHolder].question);
-    var location = questionAndAnswer[indexHolder].answers
-    $('#questionDiv p:nth-child(3)').html(location[0].questions).attr("value", location[0].result);
-    $('#questionDiv p:nth-child(4)').html(location[1].questions).attr("value", location[1].result);
-    $('#questionDiv p:nth-child(5)').html(location[2].questions).attr("value", location[2].result);
-    $('#questionDiv p:nth-child(6)').html(location[3].questions).attr("value", location[3].result);
+    currentQuestion = questionAndAnswer[indexHolder].answers
+    for (var i = 0; i <= 3; i++) {
+        var newParagraphTag = $('<p>');
+        newParagraphTag.text(currentQuestion[i].answer).attr({ value: currentQuestion[i].isCorrect, 'class': 'usersChoice' })
+        $(newParagraphTag).insertAfter($('#countDown'));
+    }
     startTimer();
 }
 
+
+
+
+// Count down for the timer
 function startTimer() {
     timer = 100;
     intervalId = setInterval(decrement, 1000);
 
 }
 
-
+// Determins if thetimmer is 0 and also decreases the time by 1 second
 function decrement() {
     if (timer === 0) {
         clearInterval(intervalId);
         incorrectGuess();
     } else {
         timer--;
-        $('#countDown').html(timer);
+        $('#countDown').text(timer);
     }
 }
 
+
 function incorrectGuess() {
     console.log("Sorry, you done messed up!");
+    incorrectAnswers++
     indexHolder++;
-    if (indexHolder <= 2) {
+    $('#question').text("Incorrect")
+    $('.usersChoice').remove();
+    newImg = $('<img src="' + currentQuestion[4] + '">');
+     $(newImg).insertAfter($('#countDown'));
+    if (indexHolder <= questionAndAnswer.length) {
         setTimeout(displayQuestion, 5000);
     }
+    else {
+    	gameFinished();
+    }
+}
+
+
+function correctGuess() {
+    console.log("Correct Answer");
+    correctAnswers++;
+    indexHolder++;
+    $('#question').text("Correct")
+    $('.usersChoice').remove();
+    newImg = $('<img src="' + currentQuestion[4] + '">');
+    $(newImg).insertAfter($('#countDown'));
+    if (indexHolder <= questionAndAnswer.length) {
+        setTimeout(displayQuestion, 5000);
+    }
+}
+
+
+function gameFinished () {
+	$('#question').text("Heres your result")	
 }
